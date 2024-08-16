cc86076004820c5a0dee9277f310c4aa75e46424
Pablo <pablogarcirodriguez@gmail.com>
2024-08-16 19:09:39 +0200

    Llamando a la API

--- src/pages/home/layout/LayoutHome.jsx
+++ src/pages/home/layout/LayoutHome.jsx
@@ -1,10 +1,25 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
+import { useEffect } from "react";
+import { URL_POKEMON } from "../../../api/apiRest";
 import Header from "../header/Header";
 import css from "./layout.module.scss";
 
 export default function LayoutHome() {
+  const [arrayPokemon, setArrayPokemon] = useState();
+
+  useEffect(() => {
+    const api = async () => {
+      const apiPoke = await axios.get(`${URL_POKEMON}`);
+      console.log(apiPoke.data);
+      setArrayPokemon(apiPoke.data.results);
+    };
+
+    api();
+  }, []);
+
   return (
-    <div>
+    <div style={css.layout}>
       <Header />
     </div>
   );
